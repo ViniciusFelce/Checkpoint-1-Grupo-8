796a1ac8eb2349340b89979a9d4b63fc65550358
Felipe Godoy <felipe-est@hotmail.com>
2022-02-23 14:56:53 -0300

    Teste criacao branch e envio do arquivo

---
+++ checkpoint.js
@@ -0,0 +1,12 @@
+// ATENÇÃO!! --  esse script deverá ser execcutado no navegador.
+//Linkar em algum .html ou chamar pelo nome do script no console do navegador.
+
+var pipoca = 10
+var macarrao = 8
+var carne = 15
+var feijao = 12
+var brigadeiro = 8
+
+opcao = []
+
+console.log
