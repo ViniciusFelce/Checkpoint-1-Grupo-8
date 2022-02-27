9439bd2cf2348c1464cbcac28afa21faeb438a2b
Felipe Godoy <felipe-est@hotmail.com>
2022-02-27 11:13:42 -0300

    checkpoint funcional finalizado, enfeitando antes de enviar o form.

--- checkpoint.js
+++ checkpoint.js
@@ -1,12 +1,43 @@
-// ATENÇÃO!! --  esse script deverá ser execcutado no navegador.
-//Linkar em algum .html ou chamar pelo nome do script no console do navegador.
+let pipoca = {
+  nome: 'pipoca',
+  defaultTime: 10
+}
 
-var pipoca = 10
-var macarrao = 8
-var carne = 15
-var feijao = 12
-var brigadeiro = 8
+let macarrao = {
+  nome: 'macarrão',
+  defaultTime: 8
+}
 
-opcao = []
+let carne = {
+  nome: 'carne',
+  defaultTime: 15
+}
 
-console.log
+let feijao = {
+  nome: 'feijão',
+  defaultTime: 12
+}
+
+let brigadeiro = {
+  nome: 'brigadeiro',
+  defaultTime: 8
+}
+
+function microwave(opcao, tempo) {
+  console.log('Você escolheu ' + opcao.nome + '!')
+  {
+    if (tempo > 2 * opcao.defaultTime && tempo <= 3 * opcao.defaultTime) {
+      console.log('O prato ' + opcao.nome + ' queimou.')
+    } else if (tempo > 3 * opcao.defaultTime) {
+      console.log(
+        'KABOOOOOOOOOOOOOOOoOoOoOoOOoOMmm!!!!!!\nVocê explodiu o microondas :('
+      )
+    } else if (tempo < opcao.defaultTime) {
+      console.log('O tempo para preparo é insuficiente.')
+    } else {
+      console.log('Prato preparado com sucesso!\nBom apetite :)')
+    }
+  }
+}
+
+microwave(carne, 14)
